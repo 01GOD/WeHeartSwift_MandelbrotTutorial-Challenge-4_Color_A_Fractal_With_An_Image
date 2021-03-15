b1d7c8cb82f19c303f8e40211390d2da2f70ac65
Robbie Moyer <rmoyer08@gmail.com>
2021-03-15 17:01:51 -0400

    Lots of changes to get image rendering working

--- MandelbrotTutorial/GameViewController.swift
+++ MandelbrotTutorial/GameViewController.swift
@@ -9,91 +9,84 @@
 import UIKit
 import SpriteKit
 
-
-
-        extension SKNode {
-            class func unarchiveFromFile(_ file : String) -> SKNode? {
-                if let url = Bundle.main.url(forResource: file, withExtension: "sks") {
-                    do {
-                        var sceneData = try NSData(contentsOfFile: url.absoluteString, options: NSData.ReadingOptions.mappedIfSafe)
-                        var archiver = NSKeyedUnarchiver(forReadingWith: sceneData as Data)
-
-                        archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
-                        let scene = archiver.decodeObject(forKey: NSKeyedArchiveRootObjectKey) as! GameScene
-                        archiver.finishDecoding()
-                        return scene
-                    } catch {
-                        return nil
-                    }
-                } else {
-                    return nil
-                }
-            }
-}
-
-//Replaced with newer edition above:
-//extension SKNode {
-//    class func unarchiveFromFile(file : String) -> SKNode? {
-//        if let path = Bundle.main.path(forResource: file, ofType: "sks") {
-//            var sceneData = NSData(contentsOfFile: path, options: NSData.ReadingOptions.mappedIfSafe)
-//
-////                contentsOfFile: path, options: .DataReadingMappedIfSafe, error: nil)!
-//            var archiver = NSKeyedUnarchiver(forReadingWithData: sceneData as Data)
-//
-//            archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
-//            let scene = archiver.decodeObjectForKey(NSKeyedArchiveRootObjectKey) as! GameScene
-//            archiver.finishDecoding()
-//            return scene
-//        } else {
-//            return nil
-//        }
-//    }
-//}
-
-
-
 class GameViewController: UIViewController, UIScrollViewDelegate {
 
-    var node:SKSpriteNode!
-    
+    var node: SKSpriteNode?
+    var skView: SKView { view as! SKView }
+
+    private var c: GLKVector2 = GLKVector2Make(0, 0)
+    private let bunnyTexture = SKTexture(imageNamed: "bunny.png")
+
     @IBOutlet weak var contentView: UIView!
     @IBOutlet weak var scrollView: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        if let scene = GameScene.unarchiveFromFile("GameScene") as? GameScene {
-            // Configure the view.
-            let skView = self.view as! SKView
-            skView.ignoresSiblingOrder = true
+        scrollView.maximumZoomScale = 100_000
 
-            /* Set the scale mode to scale to fit the window */
-            scene.scaleMode = .fill
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(didPan(_:)))
+        pan.minimumNumberOfTouches = 2
 
-            node = scene.childNode(withName: "fractal") as! SKSpriteNode
+        view.addGestureRecognizer(pan)
 
-            skView.presentScene(scene)
+        setupScene()
 
-        }
-        
         updateShader(scrollView: scrollView)
-        
-        let imageUniform = node.shader!.uniformNamed("image")!
-        
-        imageUniform.textureValue = SKTexture(imageNamed: "bunny")
     }
-    
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        print("GameViewController View Did Appear")
         updateShader(scrollView: scrollView)
+    }
+
+    @objc func didPan(_ sender: UIPanGestureRecognizer) {
+        var translation = sender.translation(in: view)
+
+        translation.x /= view.frame.size.width
+        translation.y /= view.frame.size.height
+
+        c = GLKVector2Make(Float(translation.x) + c.x, Float(translation.y) + c.y)
+
+        let cUniform = node?.shader?.uniformNamed("c")
+
+        cUniform?.floatVector2Value = c
+        sender.setTranslation(.zero, in: view)
+    }
+
+    private func setupScene() {
+        let scene = SKScene(size: CGSize(width: 1024, height: 768))
+        scene.shouldEnableEffects = true
+        scene.scaleMode = .aspectFill
+
+        let shader = SKShader(fileNamed: "Fractal.fsh")
+        shader.addUniform(SKUniform(name: "zoom", float: Float(scrollView.zoomScale)))
+        shader.addUniform(SKUniform(
+            name: "offset",
+            float: GLKVector2Make(
+                Float(scrollView.contentOffset.x),
+                Float(scrollView.contentOffset.y)
+            )
+        ))
+        shader.addUniform(SKUniform(name: "c", float: c))
+        shader.addUniform(SKUniform(name: "image", texture: bunnyTexture))
+
+        let fractalNode = SKSpriteNode(color: .red, size: scene.size)
+        fractalNode.name = "fractal"
+        fractalNode.shader = shader
+        fractalNode.anchorPoint = .zero
+
+        scene.addChild(fractalNode)
+
+        self.node = fractalNode
+
+        skView.ignoresSiblingOrder = true
+        skView.presentScene(scene)
     }
     
     func updateShader(scrollView: UIScrollView) {
-        let zoomUniform = node.shader?.uniformNamed("zoom")!
-        
-        let offsetUniform = node.shader!.uniformNamed("offset")!
+        let zoomUniform = node?.shader?.uniformNamed("zoom")
+        let offsetUniform = node?.shader?.uniformNamed("offset")
         
         var offset = scrollView.contentOffset
         
@@ -101,23 +94,18 @@ class GameViewController: UIViewController, UIScrollViewDelegate {
         offset.y /= scrollView.contentSize.height
         
         zoomUniform?.floatValue = Float(scrollView.zoomScale)
-        offsetUniform.floatVector2Value = GLKVector2Make(Float(offset.x), Float(offset.y))
+        offsetUniform?.floatVector2Value = GLKVector2Make(Float(offset.x), Float(offset.y))
     }
     
-    func scrollViewDidScroll(scrollView: UIScrollView) {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
         updateShader(scrollView: scrollView)
     }
     
-    func scrollViewDidZoom(scrollView: UIScrollView) {
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
         updateShader(scrollView: scrollView)
     }
     
-    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         return contentView
     }
-    
-//    override func prefersStatusBarHidden() -> Bool {
-//        return true
-//    }
-
 }
